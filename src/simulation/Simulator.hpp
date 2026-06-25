51b5147df611c36ebb2a86d4ac7ebf4132c1c7e1
Nebojša Kaišarević <nebojsa.kaisarevic@student.etf.unibl.org>
2026-06-25 14:59:47 +0200

    feat: dodane Gantova karta, završno stanje agenata i statistika u Simulator

--- src/simulation/Simulator.hpp
+++ src/simulation/Simulator.hpp
@@ -1,5 +1,8 @@
 #pragma once
+
+#include <map>
 #include <memory>
+#include <string>
 #include <vector>
 
 #include "../VFS/InMemoryVFS.hpp"
@@ -9,6 +12,12 @@
 #include "../scheduler/IScheduler.hpp"
 #include "EventLog.hpp"
 
+// Jedan segment Gantove karte: koji agent (prazan string = idle) i u kom intervalu
+struct GanttSegment {
+    int start;
+    int end;
+    std::string agent_id;  // prazan = idle
+};
 
 class Simulator {
     public:
@@ -24,10 +33,24 @@ class Simulator {
         std::vector<std::shared_ptr<Agent>> all_agents;
         int current_tick = 0;
 
+        // Gantova karta: slot_index -> lista segmenata
+        std::map<int, std::vector<GanttSegment>> gantt;
+        // Za pracenje sta je u svakom slotu proslog tika
+        std::vector<std::string> last_slot_agent;
+        // Odbijeni pokusaji zakljucavanja radi deadlock prevencije
+        std::vector<std::string> rejected_locks;
+
         void init ();
         void step ();
         void execute_operation (std::shared_ptr<Agent> agent);
         void handle_open (std::shared_ptr<Agent> agent, const Operation& op);
         void handle_close (std::shared_ptr<Agent> agent, const Operation& op);
         void try_unblock_agents ();
+        void update_gantt ();
+        void update_wait_times ();
+
+        void print_gantt (std::ostream& out) const;
+        void print_agent_summary (std::ostream& out) const;
+        void print_rejected_locks (std::ostream& out) const;
+        void print_statistics (std::ostream& out) const;
 };
